4756d28db2bd174cc4840cd6be598da4c2bc2e4d
jarnethorrez <jarne.thorrez@student.howest.be>
2018-01-23 14:58:20 +0100

    Finished header

--- src/view/home/index.php
+++ src/view/home/index.php
@@ -1,3 +1,4 @@
+<!-- <div class="wrapper-shower"></div> -->
 <header>
   <h1 class="hidden">Week van de mobiliteit</h1>
   <div class="left-side">
@@ -5,5 +6,12 @@
     <img src="assets/img/car.png" alt="car" class="car" align="right">
   </div>
   <div class="right-side">
+    <nav>
+      <a href="#">Over</a>
+      <a href="?page=events">acties</a>
+      <a href="?page=organiseEvent" class="button green-button">organiseer een actie</a>
+    </nav>
+    <p class="tagline">Help ons<br />om mee(r) te autominderen</p>
+    <img src="assets/img/rollerskate.png" alt="rollerskate">
   </div>
 </header>
