c6171f8ea34bd46fc2137a2521cccdebe8ca077e
jarnethorrez <jarne.thorrez@student.howest.be>
2018-01-23 16:05:41 +0100

    Added Over section to homepage

--- src/view/home/index.php
+++ src/view/home/index.php
@@ -15,3 +15,16 @@
     <img src="assets/img/rollerskate.png" alt="rollerskate">
   </div>
 </header>
+<main>
+  <section class="over">
+    <div class="inner">
+      <img src="assets/img/image-collection.png" alt="collectie van foto's">
+      <div class="text">
+        <h2>Over de week</h2>
+        <p>Elk jaar zet de Week van de Mobiliteit (16 – 22 september)
+Vlaanderen #goedopweg om mee(r) te autominderen. Tal van acties tijdens de Week doen ons stil staan bij ons verplaatsingsgedrag en laten proeven van de alternatieven. Want wie de overstap maakt ontdekt zelf de voordelen van het stappen, trappen, trein/tram/bus of autodelen.</p>
+      </div>
+      <a href="#" class="button white-outline-button wide-button">lees meer</a>
+    </div>
+  </section>
+</main>
