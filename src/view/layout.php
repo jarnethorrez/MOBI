5291b01a4f6addf68f35a04ff585eccfa714cdaa
jarnethorrez <jarne.thorrez@student.howest.be>
2018-01-23 11:59:23 +0100

    Added Futura font using WebfontLoader

--- src/view/layout.php
+++ src/view/layout.php
@@ -15,5 +15,20 @@
     </div>
 
     <?php echo $js;?>
+    <script>
+      WebFontConfig = {
+        custom: {
+          families: ['Futura'],
+          urls: ['assets/fonts/Futura/futura.css']
+        }
+      };
+
+      (function(d) {
+        var wf = d.createElement('script'), s = d.scripts[0];
+        wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+        wf.async = true;
+        s.parentNode.insertBefore(wf, s);
+      })(document);
+   </script>
   </body>
 </html>
