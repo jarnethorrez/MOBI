d072049fa7a05d8c50143bf21a47e98348f9521a
jarnethorrez <jarne.thorrez@student.howest.be>
2018-01-24 14:22:22 +0100

    Finished homepage, except for IG feed

--- src/view/home/index.php
+++ src/view/home/index.php
@@ -96,3 +96,19 @@ aangemoedigd om naar school te stappen of trappen</p>
     </div>
   </section>
 </main>
+
+<footer>
+  <div class="wrapper footer-flex">
+    <div class="info">
+      <p>De week van de mobiliteit is een initiatief van het Netwerk Duurzame Mobiliteit (Komimo vzw)</p>
+      <p>Kasteellaan 349a<br />9000 Gent</p>
+      <p>09 331 59 10</p>
+      <p>info@duurzame-mobiliteit.be</p>
+    </div>
+    <div class="social">
+      <a href="https://www.facebook.com/Weekvandemobiliteit"><img src="assets/img/facebook.png" /></a>
+      <a href="https://www.facebook.com/Weekvandemobiliteit"><img src="assets/img/instagram.png" /></a>
+      <a href="https://www.facebook.com/Weekvandemobiliteit"><img src="assets/img/twitter.png" /></a>
+    </div>
+  </div>
+</footer>
