7cb2c65b647c616a9b67adbc1301717d2f3ecc3b
jarnethorrez <jarne.thorrez@student.howest.be>
2018-01-27 16:06:17 +0100

    Getting closer and closer to working filter

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,35 @@
 const days = document.querySelectorAll(`.day`);
 const tags = document.querySelectorAll(`.tag`);
 const input = document.querySelector(`.input`);
+let selectedDaysDates;
+let selectedTagNames;
 let selectedDays = [];
+let selectedTags = [];
 
 const init = () => {
   days.forEach(day => day.addEventListener(`click`, dayClick));
   updateSelectedDays();
 
   tags.forEach(tag => tag.addEventListener(`click`, tagClick));
+  updateSelectedTags();
 
   input.addEventListener(`input`, inputInput);
 };
 
-const inputInput = e => {
-  console.log(e.currentTarget.value);
+const inputInput = () => {
+  sendToPhp();
 };
 
 const dayClick = e => {
   toggleClass(e, `day-inactive`);
   updateSelectedDays();
+  sendToPhp();
 };
 
 const tagClick = e => {
   toggleClass(e, `tag-inactive`);
+  updateSelectedTags();
+  sendToPhp();
 };
 
 const toggleClass = (e, klasse) => {
@@ -36,9 +43,33 @@ const toggleClass = (e, klasse) => {
 const updateSelectedDays = () => {
   selectedDays = Array.from(days);
   selectedDays = selectedDays.filter(day => !day.classList.contains(`day-inactive`));
-  const selectedDaysDates = [];
+  selectedDaysDates = [];
   selectedDays.forEach(day => selectedDaysDates.push(day.dataset.day));
   console.log(selectedDaysDates);
-}
+  sendToPhp();
+};
+
+const updateSelectedTags = () => {
+  selectedTags = Array.from(tags);
+  selectedTags = selectedTags.filter(tag => !tag.classList.contains(`tag-inactive`));
+  selectedTagNames = [];
+  selectedTags.forEach(tag => selectedTagNames.push(tag.innerText));
+  console.log(selectedTagNames);
+};
+
+const sendToPhp = () => {
+
+  const $form = new FormData();
+  $form.append(`action`, `search`);
+  $form.append(`days`, selectedDays);
+
+  fetch(`index.php`, {
+    headers: new Headers({
+      Accept: `application/json`,
+    }),
+    method: `post`,
+    body: $form,
+  });
+};
 
 init();
