73051d82d78965300c63da085fc85303185eb9c0
jarnethorrez <jarne.thorrez@student.howest.be>
2018-01-27 15:37:38 +0100

    Added toggleClass function, works with both tags and days

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,29 @@
 const days = document.querySelectorAll('.day');
+const tags = document.querySelectorAll('.tag');
 let selectedDays = [];
 
 const init = () => {
   days.forEach(day => day.addEventListener(`click`, dayClick));
   updateSelectedDays();
+
+  tags.forEach(tag => tag.addEventListener(`click`, tagClick));
 }
 
 const dayClick = e => {
-  if( e.currentTarget.classList.contains('day-inactive')) {
-    e.currentTarget.classList.remove('day-inactive');
-  } else {
-    e.currentTarget.classList.add('day-inactive');
-  }
+  toggleClass(e, 'day-inactive');
   updateSelectedDays();
+}
+
+const tagClick = e => {
+  toggleClass(e, 'tag-inactive');
+}
+
+const toggleClass = (e, klasse) => {
+  if( e.currentTarget.classList.contains(klasse)) {
+    e.currentTarget.classList.remove(klasse);
+  } else {
+    e.currentTarget.classList.add(klasse);
+  }
 }
 
 const updateSelectedDays = () => {
